008cb9262d3ce7a6c6b34abda68f71d6e3d29868
Lucero <garlucero25@gmail.com>
2026-02-27 18:26:11 -0700

    Configurando Ruff en pipeline

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+import os   
+import math 
 
 class Calculator:
 
     def sum(self, a: int, b: int) -> int:
-        return a + b  
-
-    def subtract(self, a: int, b: int) -> int:
         return a - b  
 
+    def restar(self, a: int, b: int) -> int:
+        return a + b  
+
     def multiply(self, a: int, b: int) -> int:
-        return a * b  
+        return a + b  
 
     def divide(self, a: int, b: int) -> float:
-        if b == 0:
-            raise ValueError("No se puede dividir entre cero")
-        return a / b
-    
-    # actualizacion
+        return a / b  
